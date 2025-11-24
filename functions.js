3fe2d989a30dbceffb6bbdb3d0b0ef7edcae0edc
kayla <koala>
2025-11-23 19:19:05 -0500

    final draft

--- functions.js
+++ functions.js
@@ -9,6 +9,7 @@ function showFrance(){
     mesopotamia.style.display = "none";
     northAmerica.style.display = "none";
     france.scrollIntoView()
+    scrollBy(0,-100)
 }
 
 function showMesopotamia(){
@@ -22,6 +23,7 @@ function showMesopotamia(){
     mesopotamia.style.display = "inline";
     northAmerica.style.display = "none";
     mesopotamia.scrollIntoView()
+    scrollBy(0,-100)
 }
 
 function showNA(){
@@ -35,4 +37,24 @@ function showNA(){
     mesopotamia.style.display = "none";
     northAmerica.style.display = "inline";
     northAmerica.scrollIntoView()
+    scrollBy(0,-100)
+}
+
+function swapPoem(){
+    var short = document.getElementById("shortPoem");
+    var long = document.getElementById("longPoem");
+    var button = document.getElementById("swapButton")
+
+    if(short.style.display === "none"){
+        long.style.display = "none";
+        short.style.display = "block";
+        button.innerHTML = "<u>Show Full Poem</u>";        
+    }
+    else {
+        short.style.display = "none";
+        long.style.display = "block";
+        button.innerHTML = "<u>Show Less</u>";
+    }
+    france.scrollIntoView()
+    scrollBy(0,-100)
 }
